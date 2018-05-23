95074932467769aa3f2fe78f1c7cbbfdec283179
Filip <fpodsiadly@gmail.com>
2018-05-23 18:52:39 +0200

    add some new packages and correct css and sass implementation

--- vue/compare-vue/src/App.vue
+++ vue/compare-vue/src/App.vue
@@ -13,13 +13,15 @@ export default {
 
 <style lang="sass">
 @import "../node_modules/bulma/bulma.sass"
+</style>
 
-  #app 
-    font-family: 'Avenir', Helvetica, Arial, sans-serif
-    -webkit-font-smoothing: antialiased
-    -moz-osx-font-smoothing: grayscale
-    text-align: center
-    color: #2c3e50
-    margin-top: 60px
-
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
