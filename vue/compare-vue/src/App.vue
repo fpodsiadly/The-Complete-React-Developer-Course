aa548d03f908a276878c1bb4d34167ae552677e1
Podsiadly <fpodsiadly@deloittece.com>
2018-05-23 16:50:25 +0200

    add few node modules, css->sass, bulma import

--- vue/compare-vue/src/App.vue
+++ vue/compare-vue/src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <router-view> </router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import "../node_modules/bulma/bulma.sass"
+1
+  #app 
+    font-family: 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
+    margin-top: 60px
 </style>
