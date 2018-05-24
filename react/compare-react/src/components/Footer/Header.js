9edd33099b88603fc42c1c635f001450180693c9
Filip <fpodsiadly@gmail.com>
2018-05-24 17:37:57 +0200

    add bulma css framework, add new components

---
+++ react/compare-react/src/components/Footer/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Footer = () => {
+
+    return (
+      <div className="App">
+       My footer
+      </div>
+    );
+  };
+
+
+export default Footer;
