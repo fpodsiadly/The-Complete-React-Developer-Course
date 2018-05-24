441430aebe3dab716a9e6131717e361cfdc10f32
Filip <fpodsiadly@gmail.com>
2018-05-24 12:22:53 +0200

    add mq sass

--- vue/compare-vue/src/App.vue
+++ vue/compare-vue/src/App.vue
@@ -12,7 +12,8 @@ export default {
 </script>
 
 <style lang="sass">
-@import "../node_modules/bulma/bulma.sass"
+@import '../node_modules/bulma/bulma.sass'.
+@import 'mq'
 
 #app 
   font-family: "Avenir", Helvetica, Arial, sans-serif
