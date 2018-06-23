867e575159c582f29601afe478f68a3407c1906e
Filip <fpodsiadly@gmail.com>
2018-06-23 18:26:32 +0200

    install and import react

--- react/udemy/indecision-app/src/app.js
+++ react/udemy/indecision-app/src/app.js
@@ -1,12 +1,7 @@
 //import './utils.js';
-import subtract, { square,add } from './utils.js';
-import isSenior, {isAdult,canDrink} from './person';
 
-console.log('app.js is running!!');
-console.log(square(4));
-console.log(add(1,2));
-console.log(subtract(100,81));
+import React from 'react';
+import ReacDOM from 'react-dom';
 
-console.log(isAdult(28));
-console.log(canDrink(28));
-console.log(isSenior(65));
+const template = React.createElement('p',{},'testing 123');
+ReacDOM.render(template,document.getElementById('app'));
