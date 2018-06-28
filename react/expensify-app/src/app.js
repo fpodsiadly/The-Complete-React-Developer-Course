e7a78f0729537059cac32d492fe548003a3b4f0b
Filip <fpodsiadly@gmail.com>
2018-06-28 12:46:53 +0200

    add routing, create few subpages

--- react/expensify-app/src/app.js
+++ react/expensify-app/src/app.js
@@ -1,6 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import "./styles/style.scss";
 
-ReactDOM.render(<p>This is my bilerplate</p>, document.getElementById("app"));
+const ExpenseDashoardPage = () => (
+    <div>
+        This is from my darhboard component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is from my Add Expense component
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is from my Edit Expense component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is from my HelpPage component
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashoardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+
+
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
