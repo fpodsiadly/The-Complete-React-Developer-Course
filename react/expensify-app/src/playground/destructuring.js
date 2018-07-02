9c45612f73eda4d57b666fb791c975e2c3b2f477
Filip <fpodsiadly@gmail.com>
2018-07-02 09:27:13 +0200

    Array destructuring

--- react/expensify-app/src/playground/destructuring.js
+++ react/expensify-app/src/playground/destructuring.js
@@ -1,3 +1,5 @@
+//Object destructuring
+
 const person = {
   //name: "Filip",
   age: 28,
@@ -14,7 +16,7 @@ const { name: firstName = 'Anonymouse', age } = person;  //default is no value
 
 console.log(`${firstName} is ${age}.`);
 
-const { city, temp:temperature } = person.location;  //rename
+const { city, temp: temperature } = person.location;  //rename
 if (city && temperature) {
   console.log(`Its's ${temperature} in ${city} `);
 }
@@ -27,6 +29,20 @@ const book = {
   }
 };
 
-const {name: publisherName = 'Self-Published'} = book.publisher;
+const { name: publisherName = 'Self-Published' } = book.publisher;
 
 console.log(publisherName); //Penguin or defaut Self-Published
+
+//Array destructuring
+
+const address = ['1299 S Juniper Street', 'Lodz', 'Lodzkie', '19147'];
+
+const [, cities, state = 'New York'] = address;  //default value
+
+//console.log(`You are in ${address[1]} ${address[2]}.`);
+console.log(`You are in ${cities} ${state}.`);
+
+const item = ['coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [item1, , item3] = item;
+console.log(`A medium ${item1} costs ${item3}`);
