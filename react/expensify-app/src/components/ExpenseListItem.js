68bac4985a746233766e75d8789986d05734f33c
Filip <fpodsiadly@gmail.com>
2018-07-17 12:45:31 +0200

    add sortby select

--- react/expensify-app/src/components/ExpenseListItem.js
+++ react/expensify-app/src/components/ExpenseListItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {removeExpense} from '../actions/expenses';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
-   <h3> {description}</h3>
+    <h3> {description}</h3>
     <p> {amount} - {createdAt}</p>
     <button onClick={() => {
       dispatch(removeExpense({ id }));
@@ -12,4 +12,4 @@ const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
   </div>
 );
 
-export default connect ()(ExpenseListItem);
+export default connect()(ExpenseListItem);
