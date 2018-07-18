d25b0a9c64f8fc70f086887cfe3b77241d5e6ba1
Filip <fpodsiadly@gmail.com>
2018-07-18 16:22:09 +0200

    configure jestjs and write first tests

---
+++ react/expensify-app/src/tests/add.test.js
@@ -0,0 +1,19 @@
+const add = (a, b) => a + b;
+const generatorGreeting = (name = 'Anonymous') => `Hello ${name}!`;
+
+
+test('should generate greeting from name', () => {
+  const result = generatorGreeting('Filip');
+  expect(result).toBe('Hello Filip!')
+});
+
+
+test('should add two numbers', () => {
+  const result = add(3, 4);
+  expect(result).toBe(7);
+});
+
+test('should generate greeting for no name', () => {
+  const result = generatorGreeting();
+  expect(result).toBe('Hello Anonymous!');
+});
