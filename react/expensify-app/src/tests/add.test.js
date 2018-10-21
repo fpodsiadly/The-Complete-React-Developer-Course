85807bc8623091c26c86ab3d14636aa9cc911f3a
Filip <fpodsiadly@gmail.com>
2018-10-21 12:58:53 +0200

    add testing actions

--- react/expensify-app/src/tests/add.test.js
+++ react/expensify-app/src/tests/add.test.js
@@ -1,16 +1,14 @@
 const add = (a, b) => a + b;
 const generatorGreeting = (name = 'Anonymous') => `Hello ${name}!`;
 
+test('should add two numbers', () => {
+  const result = add(3, 4);
+  expect(result).toBe(7);
+});
 
 test('should generate greeting from name', () => {
   const result = generatorGreeting('Filip');
   expect(result).toBe('Hello Filip!')
-});
-
-
-test('should add two numbers', () => {
-  const result = add(3, 4);
-  expect(result).toBe(7);
 });
 
 test('should generate greeting for no name', () => {
